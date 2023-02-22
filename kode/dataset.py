4d1627472ce1fa1647e1ec3fc0fee4ec5093f19c
Helene Semb <helensem@stud.ntnu.no>
2023-02-22 12:12:16 +0100

    fix image_id

--- kode/dataset.py
+++ kode/dataset.py
@@ -63,6 +63,7 @@ def load_damage_dicts(dataset_dir, subset): #? Possibly write this to a JSON-fil
     assert subset in ["train", "val"]
     dataset_dir = os.path.join(dataset_dir, subset)
     image_ids = next(os.walk(dataset_dir))[1]
+    idx = 0
     for image_id in image_ids:
 
         image_dir = os.path.join(dataset_dir, image_id)
@@ -73,8 +74,8 @@ def load_damage_dicts(dataset_dir, subset): #? Possibly write this to a JSON-fil
         image_path = os.path.join(image_dir, file_name)
         print(image_path)
         height, width = cv2.imread(image_path).shape[:2]
-        record = create_image_annotation(image_path, width, height, image_id)
-
+        record = create_image_annotation(image_path, width, height, idx)
+        idx +=1
         mask_dir = os.path.join(image_dir, 'masks')
         objs = []
         for f in next(os.walk(mask_dir))[2]:
