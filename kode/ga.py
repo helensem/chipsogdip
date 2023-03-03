cc6ec797dcd0589b36e0d2760530dd27ab45b8c5
Helene Semb <helensem@stud.ntnu.no>
2023-03-03 16:12:01 +0100

    something new

--- kode/ga.py
+++ kode/ga.py
@@ -35,7 +35,7 @@ from detectron2.engine import DefaultPredictor
 #    dataset = dataset.batch(batch_size).prefetch(1)
 #    return dataset
 
-def evaluate(hyperparameters):
+def calculate_fitness(hyperparameters):
     #####
     ##### CHANGE HERE #####
     ######
@@ -161,50 +161,161 @@ def mutate_random_hyperparameters_full(hyperparameters):
 #     return model
 
 
-###### CHANGE ########
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
+####* CODE FROM KNAPSACK PROBLEM
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMin)
+def generate_hyperparameters(): 
+    init_values = {} 
+    init_values["RPN_ANCHOR_STRIDE"] = np.array([1,2,3,4])
+    init_values["RPN_NMS_THRESHOLD"] = np.linspace(0.5, 1)
+    init_values["RPN_TRAIN_ANCHORS_PER_IMAGE"] = np.array([64, 128, 256, 512, 1024])
+    init_values["PRE_NMS_LIMIT"] = np.linspace(4000,8000,dtype=int)
+    init_values["POST_NMS_ROIS_TRAINING"] = np.linspace(1000,3000,dtype=int)
+    init_values["POST_NMS_ROIS_INFERENCE"] = np.linspace(600,2000,dtype=int)
+    init_values["MEAN_PIXEL"] = np.array([np.linspace(115.0,130.0,), 
+                                            np.linspace(110.0,125.0),
+                                            np.linspace(95.0,115.0)])
+    print(init_values["MEAN_PIXEL"]) 
+    init_values["TRAIN_ROIS_PER_IMAGE"] = np.linspace(150,500,dtype=int)
+    init_values["ROI_POSITIVE_RATIO"] = np.linspace(0.2, 0.5)
+    init_values["MAX_GT_INSTANCES"] = np.linspace(70,400,dtype=int)
+    init_values["DETECTION_MAX_INSTANCES"] = np.linspace(70,400,dtype=int)
+    init_values["DETECTION_MIN_CONFIDENCE"] = np.linspace(0.3,0.9)
+    init_values["DETECTION_NMS_THRESHOLD"] = np.linspace(0.2,0.7)
+    init_values["LEARNING_MOMENTUM"] = np.linspace(0.75,0.95)
+    init_values["WEIGHT_DECAY"] = np.linspace(0.00007, 0.000125)
+    init_values["rpn_class_loss"] = np.linspace(1,10)
+    init_values["rpn_bbox_loss"] = np.linspace(1,10)
+    init_values["mrcnn_class_loss"] = np.linspace(1,10)
+    init_values["mrcnn_bbox_loss"] = np.linspace(1,10)
+    init_values["mrcnn_mask_loss"] = np.linspace(1,10)
+    return init_values
 
-toolbox = base.Toolbox()
-toolbox.register("hyperparameters", generate_random_hyperparameters)
-toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.hyperparameters)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("evaluate", evaluate)
 
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
-toolbox.register("select", tools.selTournament, tournsize=3)
-"""
-NGEN = 50
-MU = 10
-LAMBDA = 50
-CXPB = 0.5
-MUTPB = 0.1"""
+def generate_population(size):
+	population = []
+	for _ in range(size):
+		genes = [0, 1]
+		chromosome = []
+		for _ in range(len(items)):
+			chromosome.append(random.choice(genes))
+		population.append(chromosome)
+	print("Generated a random population of size", size)
+	return population
 
 
-NGEN = 2
-MU = 2
-LAMBDA = 5
-CXPB = 0.5
-MUTPB = 0.1
+def select_chromosomes(population):
+	fitness_values = []
+	for chromosome in population:
+		fitness_values.append(calculate_fitness(chromosome))
+	
+	fitness_values = [float(i)/sum(fitness_values) for i in fitness_values]
+	
+	parent1 = random.choices(population, hyperparameters=fitness_values, k=1)[0]
+	parent2 = random.choices(population, hyperparameters=fitness_values, k=1)[0]
+	
+	print("Selected two chromosomes for crossover")
+	return parent1, parent2
 
-print(toolbox.population)
+def crossover(parent1, parent2):
+	crossover_point = random.randint(0, len(parent1)-1)
+	child1 = parent1[0:crossover_point] + parent2[crossover_point:]
+	child2 = parent2[0:crossover_point] + parent1[crossover_point:]
+	
+	print("Performed crossover between two chromosomes")
+	return child1, child2
 
-pop = toolbox.population(n=MU)
-hof = tools.HallOfFame(1)
-stats = tools.Statistics(lambda ind: ind.fitness.values)
-stats.register("avg", np.mean)
-stats.register("std", np.std)
-stats.register("min", np.min)
-stats.register("max", np.max)
+def mutate(chromosome):
+	mutation_point = random.randint(0, len(chromosome)-1)
+	if chromosome[mutation_point] == 0:
+		chromosome[mutation_point] = 1
+	else:
+		chromosome[mutation_point] = 0
+	print("Performed mutation on a chromosome")
+	return chromosome
 
-pop, log = deap.algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats=stats, halloffame=hof, verbose=True)
 
-best_hyperparameters = hof[0]
+def get_best(population):
+	fitness_values = []
+	for chromosome in population:
+		fitness_values.append(calculate_fitness(chromosome))
 
-best_inds = [ind for ind in hof.items]
-best_inds_df = pd.DataFrame(best_inds)
-best_inds_df.to_csv(r"", index=False)
-print("Best hyperparameters: ", best_hyperparameters)
+	max_value = max(fitness_values)
+	max_index = fitness_values.index(max_value)
+	return population[max_index]
+
+
+mutation_probability = 0.2
+generations = 20 
+hyperparameters = generate_hyperparameters()
+population_size = 20
+
+population = [dict(zip(hyperparameters.keys(), [random.choice(values) for values in hyperparameters.values()])) for _ in range(population_size)]
+
+# for _ in range(generations):
+# 	# select two chromosomes for crossover
+# 	parent1, parent2 = select_chromosomes(population)
+
+# 	# perform crossover to generate two new chromosomes
+# 	child1, child2 = crossover(parent1, parent2)
+
+# 	# perform mutation on the two new chromosomes
+# 	if random.uniform(0, 1) < mutation_probability:
+# 		child1 = mutate(child1)
+# 	if random.uniform(0, 1) < mutation_probability:
+# 		child2 = mutate(child2)
+
+# 	# replace the old population with the new population
+# 	population = [child1, child2] + population[2:]
+
+if __name__ == "__main__":
+    print(population)
+     
+
+
+
+
+
+# creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+# creator.create("Individual", list, fitness=creator.FitnessMin)
+
+# toolbox = base.Toolbox()
+# toolbox.register("hyperparameters", generate_random_hyperparameters)
+# toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.hyperparameters)
+# toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+# toolbox.register("evaluate", evaluate)
+
+# toolbox.register("mate", tools.cxTwoPoint)
+# toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
+# toolbox.register("select", tools.selTournament, tournsize=3)
+# """
+# NGEN = 50
+# MU = 10
+# LAMBDA = 50
+# CXPB = 0.5
+# MUTPB = 0.1"""
+
+
+# NGEN = 2
+# MU = 2
+# LAMBDA = 5
+# CXPB = 0.5
+# MUTPB = 0.1
+
+# print(toolbox.population)
+
+# pop = toolbox.population(n=MU)
+# hof = tools.HallOfFame(1)
+# stats = tools.Statistics(lambda ind: ind.fitness.values)
+# stats.register("avg", np.mean)
+# stats.register("std", np.std)
+# stats.register("min", np.min)
+# stats.register("max", np.max)
+
+# pop, log = deap.algorithms.eaMuPlusLambda(pop, toolbox, MU, LAMBDA, CXPB, MUTPB, NGEN, stats=stats, halloffame=hof, verbose=True)
+
+# best_hyperparameters = hof[0]
+
+# best_inds = [ind for ind in hof.items]
+# best_inds_df = pd.DataFrame(best_inds)
+# best_inds_df.to_csv(r"", index=False)
+# print("Best hyperparameters: ", best_hyperparameters)
