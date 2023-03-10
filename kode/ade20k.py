b3bb710f7e0bd43835b2c7bca813017131a93c58
Helene Semb <helensem@stud.ntnu.no>
2023-03-10 13:03:09 +0100

    lets see

--- kode/ade20k.py
+++ kode/ade20k.py
@@ -4,11 +4,117 @@ import shutil
 #import argparse
 #import zipfile
 #from gluoncv.utils import download, makedirs
-from gluoncv.data import ADE20KSegmentation
+#from gluoncv.data import ADE20KSegmentation
 
 
 _TARGET_DIR = os.path.expanduser('~/.mxnet/datasets/ade')
 
+"""Pascal ADE20K Semantic Segmentation Dataset."""
+import os
+from PIL import Image
+import numpy as np
+import mxnet as mx
+from gluoncv.data.segbase import SegmentationDataset
+
+class ADE20KSegmentation(SegmentationDataset):
+    """ADE20K Semantic Segmentation Dataset.
+    Parameters
+    ----------
+    root : string
+        Path to VOCdevkit folder. Default is '$(HOME)/mxnet/datasplits/ade'
+    split: string
+        'train', 'val' or 'test'
+    transform : callable, optional
+        A function that transforms the image
+    Examples
+    --------
+    >>> from mxnet.gluon.data.vision import transforms
+    >>> # Transforms for Normalization
+    >>> input_transform = transforms.Compose([
+    >>>     transforms.ToTensor(),
+    >>>     transforms.Normalize([.485, .456, .406], [.229, .224, .225]),
+    >>> ])
+    >>> # Create Dataset
+    >>> trainset = gluoncv.data.ADE20KSegmentation(split='train', transform=input_transform)
+    >>> # Create Training Loader
+    >>> train_data = gluon.data.DataLoader(
+    >>>     trainset, 4, shuffle=True, last_batch='rollover',
+    >>>     num_workers=4)
+    """
+    # pylint: disable=abstract-method
+    BASE_DIR = 'ADEChallengeData2016'
+    NUM_CLASS = 1
+    CLASSES = ("sky")
+    def __init__(self, root=os.path.expanduser('~/.mxnet/datasets/ade'),
+                 split='train', mode=None, transform=None, **kwargs):
+        super(ADE20KSegmentation, self).__init__(root, split, mode, transform, **kwargs)
+        root = os.path.join(root, self.BASE_DIR)
+        assert os.path.exists(root), "Please setup the dataset using" + \
+            "scripts/datasets/ade20k.py"
+        self.images, self.masks = _get_ade20k_pairs(root, split)
+        assert (len(self.images) == len(self.masks))
+        if len(self.images) == 0:
+            raise(RuntimeError("Found 0 images in subfolders of: \
+                " + root + "\n"))
+
+    def __getitem__(self, index):
+        img = Image.open(self.images[index]).convert('RGB')
+        if self.mode == 'test':
+            img = self._img_transform(img)
+            if self.transform is not None:
+                img = self.transform(img)
+            return img, os.path.basename(self.images[index])
+        mask = Image.open(self.masks[index])
+        # synchrosized transform
+        if self.mode == 'train':
+            img, mask = self._sync_transform(img, mask)
+        elif self.mode == 'val':
+            img, mask = self._val_sync_transform(img, mask)
+        else:
+            assert self.mode == 'testval'
+            img, mask = self._img_transform(img), self._mask_transform(mask)
+        # general resize, normalize and toTensor
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, mask
+
+    def _mask_transform(self, mask):
+        return mx.nd.array(np.array(mask), mx.cpu(0)).astype('int32') - 1
+
+    def __len__(self):
+        return len(self.images)
+
+    @property
+    def classes(self):
+        """Category names."""
+        return type(self).CLASSES
+
+    @property
+    def pred_offset(self):
+        return 1
+
+def _get_ade20k_pairs(folder, mode='train'):
+    img_paths = []
+    mask_paths = []
+    if mode == 'train':
+        img_folder = os.path.join(folder, 'images/training')
+        mask_folder = os.path.join(folder, 'annotations/training')
+    else:
+        img_folder = os.path.join(folder, 'images/validation')
+        mask_folder = os.path.join(folder, 'annotations/validation')
+    for filename in os.listdir(img_folder):
+        basename, _ = os.path.splitext(filename)
+        if filename.endswith(".jpg"):
+            imgpath = os.path.join(img_folder, filename)
+            maskname = basename + '.png'
+            maskpath = os.path.join(mask_folder, maskname)
+            if os.path.isfile(maskpath):
+                img_paths.append(imgpath)
+                mask_paths.append(maskpath)
+            else:
+                print('cannot find the mask:', maskpath)
+
+    return img_paths, mask_paths
 
 # def parse_args():
 #     parser = argparse.ArgumentParser(
@@ -36,10 +142,6 @@ if __name__ == '__main__':
     root = r"/cluster/home/helensem/Master/ade"
     train_dataset = ADE20KSegmentation(root,split='train')
     val_dataset = ADE20KSegmentation(root, split='val')
-    train_dataset.CLASSES = {"sky"}
-    val_dataset.CLASSES = {"sky"}
-    train_dataset.NUM_CLASS = 1
-    val_dataset.NUM_CLASS = 1
     print('Training images:', len(train_dataset))
     print('Validation images:', len(val_dataset))
     print("Classes:", train_dataset.classes)
