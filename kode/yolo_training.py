e7cd0af1f1a10ca8701c18b1b7d636cb950c1ec6
Helene Semb <helensem@stud.ntnu.no>
2023-03-13 11:40:12 +0100

    test

--- kode/yolo_training.py
+++ kode/yolo_training.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 
 yolo_model = YOLO("yolov8n-seg.pt")
-yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml", epochs = 30, patience = 25)
+yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/coco8-seg/data.yaml", epochs = 30, patience = 25)
 
