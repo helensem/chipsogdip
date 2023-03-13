8bab13bf89621cb5fb2a68faec570113e90401a3
Helene Semb <helensem@stud.ntnu.no>
2023-03-13 11:00:00 +0100

    yolo

--- kode/yolo_training.py
+++ kode/yolo_training.py
@@ -1,28 +1,6 @@
 from ultralytics import YOLO 
-import cv2 
-import numpy as np 
-from detectron2.engine import DefaultPredictor 
-from detectron2.evaluation import COCOEvaluator, inference_on_dataset 
-from detectron2.data import build_detection_test_loader 
-
-yolo_model = YOLO("yolov8n.pt")
-yolo_model.train()
 
 
+yolo_model = YOLO("yolov8n-seg.pt")
+yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml", epochs = 30, patience = 25)
 
-
-# FROM CHAT GPT 
-image = cv2.imread("input_image.jpg")
-boxes = yolo_model.detect_bridges(image)
-
-
-
-
-rois = [] 
-for box in boxes: 
-    x1, y1, x2, y2 = box 
-    roi = image[y1:y2, x1:x2]
-    rois.append(roi)
-
-mask_rcnn_model = DefaultPredictor()
- 
