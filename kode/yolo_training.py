5612dcececa2f2bf8da98141136db6c711a89e87
Helene Semb <helensem@stud.ntnu.no>
2023-03-20 10:33:03 +0100

    train for more epochs

--- kode/yolo_training.py
+++ kode/yolo_training.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 
 yolo_model = YOLO("yolov8n-seg.pt")
-yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml", epochs = 30, patience = 25)
+yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml", epochs = 40, patience = 35)
 
