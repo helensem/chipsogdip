bd197fae119ec57212323319e83dad6910655270
Helene Semb <helensem@stud.ntnu.no>
2023-03-28 12:53:54 +0200

    love my paths

--- kode/yolo_training.py
+++ kode/yolo_training.py
@@ -7,5 +7,5 @@ import cv2
 
 model_pred = YOLO("/cluster/home/helensem/Master/runs/segment/train9/weights/best.pt")
 
-im2 = cv2.imread("/cluster/home/helensem/Master/Labeled_pictures/val/img6.jpg")
+im2 = cv2.imread("/cluster/home/helensem/Master/Labeled_pictures/val/img6/img6.jpg")
 results = model_pred.predict(source=im2, save=True, save_txt=True)  # save predictions as labels
