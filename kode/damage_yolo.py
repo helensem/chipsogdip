7700b7e6bb71a96a0b8d36132a4982dd5da5ab95
Helene Semb <helensem@stud.ntnu.no>
2023-03-31 12:13:06 +0200

    train for more epochs

--- kode/damage_yolo.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO 
-
-
-yolo_model = YOLO("yolov8n-seg.pt")
-yolo_model.train(data = "/cluster/home/helensem/Master/damage_data/data.yaml", epochs = 30, patience = 25)
--- kode/yolo_training.py
+++ kode/yolo_training.py
@@ -4,53 +4,61 @@ import cv2
 import numpy as np
 import os
 
-#yolo_model = YOLO("yolov8n-seg.pt")
-#yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml", epochs = 40, patience = 35)
 
 
-model_pred = YOLO("/cluster/home/helensem/Master/runs/segment/train9/weights/best.pt")
+if __name__ == "__main__": 
+    mode = "train"
 
-path = r"/cluster/home/helensem/Master/Labeled_pictures/test"
-image_ids = next(os.walk(path))[2]
+    if mode == "train":
 
-for id in image_ids: 
-    image_path = os.path.join(path, id) 
+        yolo_model = YOLO("yolov8n-seg.pt")
+        yolo_model.train(data = "/cluster/home/helensem/Master/sky_data/data.yaml")
 
-    image = cv2.imread(image_path)
-    #height, widht = image.shape[:2]
-    results = model_pred.predict(source=image, save=True, save_txt=True)  # save predictions as labels
 
-    #print(results)
-    for result in results: 
-        masks = result.masks.masks.cpu().numpy()     # masks, (N, H, W)
-        masks = np.moveaxis(masks, 0, -1) # masks, (H, W, N)
-        # rescale masks to original image
-        masks = scale_image(masks.shape[:2], masks, result.masks.orig_shape)
-        masks = np.moveaxis(masks, -1, 0) # masks, (N, H, W)
-        #cv2.imwrite(r"/cluster/home/helensem/Master/output/sky/test.jpg", (result.masks.masks[0].cpu().numpy()*255).astype("uint8"))
-        #print(masks)
+    elif mode == "predict":
+        model_pred = YOLO("/cluster/home/helensem/Master/runs/segment/train9/weights/best.pt")
 
-        for mask in masks:
-            mask = (mask*255).astype("uint8")
-            print(mask.shape)
-            print(image.shape)
-            #print(mask)
-            #binary_mask = cv2.threshold(mask, 0.5, 1, cv2.THRESH_BINARY)[1]
-            #print(binary_mask)
-            # Convert the binary mask to the same datatype as the image
-            #mask = mask.astype(np.uint8)
-            mask = cv2.bitwise_not(mask)
-            image = cv2.bitwise_and(image, image, mask=mask)
+        path = r"/cluster/home/helensem/Master/Labeled_pictures/test"
+        image_ids = next(os.walk(path))[2]
 
-    # load the original input image and display it to our screen
-    # a mask is the same size as our image, but has only two pixel
-    # values, 0 and 255 -- pixels with a value of 0 (background) are
-    # ignored in the original image while mask pixels with a value of
-    # 255 (foreground) are allowed to be kept
-    #mask = np.zeros(image.shape[:2], dtype="uint8")
-    #cv2.rectangle(mask, (0, 90), (290, 450), 255, -1)
-    #cv2.imshow("Rectangular Mask", masks)
-    # apply our mask -- notice how only the person in the image is
-    # cropped out
-        destination = os.path.join(r"/cluster/home/helensem/Master/output/sky", id)
-        cv2.imwrite(destination, image)
+        for id in image_ids: 
+            image_path = os.path.join(path, id) 
+
+            image = cv2.imread(image_path)
+            #height, widht = image.shape[:2]
+            results = model_pred.predict(source=image, save=True, save_txt=True)  # save predictions as labels
+
+            #print(results)
+            for result in results: 
+                masks = result.masks.masks.cpu().numpy()     # masks, (N, H, W)
+                masks = np.moveaxis(masks, 0, -1) # masks, (H, W, N)
+                # rescale masks to original image
+                masks = scale_image(masks.shape[:2], masks, result.masks.orig_shape)
+                masks = np.moveaxis(masks, -1, 0) # masks, (N, H, W)
+                #cv2.imwrite(r"/cluster/home/helensem/Master/output/sky/test.jpg", (result.masks.masks[0].cpu().numpy()*255).astype("uint8"))
+                #print(masks)
+
+                for mask in masks:
+                    mask = (mask*255).astype("uint8")
+                    print(mask.shape)
+                    print(image.shape)
+                    #print(mask)
+                    #binary_mask = cv2.threshold(mask, 0.5, 1, cv2.THRESH_BINARY)[1]
+                    #print(binary_mask)
+                    # Convert the binary mask to the same datatype as the image
+                    #mask = mask.astype(np.uint8)
+                    mask = cv2.bitwise_not(mask)
+                    image = cv2.bitwise_and(image, image, mask=mask)
+
+            # load the original input image and display it to our screen
+            # a mask is the same size as our image, but has only two pixel
+            # values, 0 and 255 -- pixels with a value of 0 (background) are
+            # ignored in the original image while mask pixels with a value of
+            # 255 (foreground) are allowed to be kept
+            #mask = np.zeros(image.shape[:2], dtype="uint8")
+            #cv2.rectangle(mask, (0, 90), (290, 450), 255, -1)
+            #cv2.imshow("Rectangular Mask", masks)
+            # apply our mask -- notice how only the person in the image is
+            # cropped out
+                destination = os.path.join(r"/cluster/home/helensem/Master/output/sky", id)
+                cv2.imwrite(destination, image)
