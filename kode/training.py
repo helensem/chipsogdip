7ffe847d3743476f0b876eb57642a95c1bbfaf72
Helene Semb <helensem@stud.ntnu.no>
2023-04-20 14:07:38 +0200

    with segment sky

--- kode/training.py
+++ kode/training.py
@@ -80,7 +80,7 @@ def config():
 
 
 
-    cfg.OUTPUT_DIR = "/cluster/work/helensem/Master/output/run1/resnet101" #! MUST MATCH WITH CURRENT MODEL 
+    cfg.OUTPUT_DIR = "/cluster/work/helensem/Master/output/run1/resnet50" #! MUST MATCH WITH CURRENT MODEL 
 
     return cfg 
  
@@ -90,7 +90,7 @@ def config():
 #experiment.log_parameters(hyper_params)
 
 if __name__ == "__main__":
-    mode = "inference"
+    mode = "evaluate"
     for d in ["train", "val"]:
         DatasetCatalog.register("damage_" + d, lambda d=d: load_damage_dicts(r"/cluster/home/helensem/Master/Labeled_pictures",d))
         MetadataCatalog.get("damage_" + d).set(thing_classes=["damage"])
@@ -154,7 +154,7 @@ if __name__ == "__main__":
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
 
-        evaluate_model(cfg, val_dict, write_to_file = True, segment_sky=False) 
+        evaluate_model(cfg, val_dict, write_to_file = True, segment_sky=True) 
 
     else: 
         print("No mode chosen")
