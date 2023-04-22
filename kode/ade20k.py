1db46fc89ddef3b62c93de21d00b02b3d0fe2f52
Helene Semb <helensem@stud.ntnu.no>
2023-04-22 15:15:30 +0200

    multi_class

--- kode/ade20k.py
+++ kode/ade20k.py
@@ -48,25 +48,37 @@ def load_sky_yolo(root, subset,destination):
         #print(mask)
         #print(mask.dtype)
         string = ""
-        mask = np.where(mask==3, 255,0) #3 for sky
-        if 255 not in mask: 
-            continue
-        mask = mask.astype('uint8')
+        for label in [3, 5, 10, 22]:
+            mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+            height, width = mask.shape
+            mask = np.where(mask==3, 255,0) #3 for sky
+            if 255 not in mask: 
+                continue
+            mask = mask.astype('uint8')
         #print(mask)
 
-        contours = find_contours(mask)
-        for contour in contours:
-            contour_list = contour.flatten().tolist()
-            if len(contour_list) < 5:
-                continue
-            string += "0 "
-        #print(mask.shape)
-            for i in range(1,len(contour_list),2): 
-                string += str(round(contour_list[i-1]/width,6)) #x coordinate
-                string += " "
-                string += str(round(contour_list[i]/height, 6)) # y coordinate
-                string += " "
-            string+= "\n"
+            contours = find_contours(mask)
+            for contour in contours:
+                contour_list = contour.flatten().tolist()
+                if len(contour_list) < 5:
+                    continue
+                if label == 3: 
+                    string += "0 "
+                elif label == 5: 
+                    string += "1 "
+                elif label == 10:
+                    string += "2 "
+                elif label == 22: 
+                    string += "3 "
+            #print(mask.shape)
+                for i in range(1,len(contour_list),2): 
+                    string += str(round(contour_list[i-1]/width,6)) #x coordinate
+                    string += " "
+                    string += str(round(contour_list[i]/height, 6)) # y coordinate
+                    string += " "
+                string+= "\n"
+        if string == "": 
+            continue
         image_id = os.path.splitext(id)[0] + '.jpg'
         image_source = os.path.join(source, image_id)
         image_dest = os.path.join(destination, "images", subset, image_id)
@@ -83,18 +95,18 @@ def load_sky_yolo(root, subset,destination):
 
 if __name__ == '__main__':
     
-    destination = r"/cluster/home/helensem/Master/ade_mask.png"
-    root = r"/cluster/home/helensem/Master/ade/ADEChallengeData2016/annotations/training/ADE_train_00000004.png"
+    destination = r"/cluster/home/helensem/Master/sky_data_multi"
+    root = r"/cluster/home/helensem/Master/ade/ADEChallengeData2016"
 
-    mask = cv2.imread(root, cv2.IMREAD_GRAYSCALE)
-    #print(mask)
-    #print(mask.dtype)
-    mask = np.where(mask==3, 255,0) #3 for sky
-    mask = mask.astype('uint8')
-    cv2.imwrite(destination, mask)
+    # mask = cv2.imread(root, cv2.IMREAD_GRAYSCALE)
+    # #print(mask)
+    # #print(mask.dtype)
+    # mask = np.where(mask==3, 255,0) #3 for sky
+    # mask = mask.astype('uint8')
+    # cv2.imwrite(destination, mask)
 
-    #for d in ['train', 'val']:
-     #   load_sky_yolo(root, d, destination)
+    for d in ['train', 'val']:
+        load_sky_yolo(root, d, destination)
     
 
 
