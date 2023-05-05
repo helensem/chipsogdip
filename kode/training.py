b34bcdb6854fe058acbdff78979e8ca83a5d5af0
Helene <helensem@stud.ntnu.no>
2023-05-05 19:00:59 +0200

    boom

--- kode/training.py
+++ kode/training.py
@@ -103,7 +103,7 @@ def config():
 #experiment.log_parameters(hyper_params)
 
 if __name__ == "__main__":
-    mode = "train"
+    mode = "evaluate"
     for d in ["train", "val"]:
         DatasetCatalog.register("damage_" + d, lambda d=d: load_damage_dicts(r"/cluster/home/helensem/Master/damage_data",d, segment_sky=False))
         MetadataCatalog.get("damage_" + d).set(thing_classes=["red corrosion"])
@@ -142,7 +142,7 @@ if __name__ == "__main__":
         cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
         cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.8
 
-        evaluate_model(cfg, val_dict, write_to_file = True, plot=False, segment_sky=False) 
+        evaluate_model(cfg, val_dict, write_to_file = True, plot=True, segment_sky=False) 
 
     elif mode == "inference": 
         val_dict = load_damage_dicts(r"/cluster/home/helensem/Master/damage_data", "val")
